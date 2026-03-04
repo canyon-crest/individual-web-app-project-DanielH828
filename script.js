f846dbf8e60212c127d8c3ca54e3852a2bf7762f
DanielH828 <hed3272@my.sduhsd.net>
2026-03-04 19:51:52 +0000

    3/4/26 minor change

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-// add javascript here
+window.onload = function() {
+
+    const button = document.querySelector("button");
+    const form = document.getElementById("contactForm");
+    const recentMessages = document.getElementById("recentMessages");
+
+    button.addEventListener("click", function(event) {
+
+        event.preventDefault(); // FORCE stop reload
+
+        const name = document.getElementById("name").value;
+        const message = document.getElementById("message").value;
+
+        const messageDiv = document.createElement("div");
+        messageDiv.classList.add("message-item");
+
+        messageDiv.innerHTML = `
+            <strong>${name}</strong>
+            <p>${message}</p>
+            <span>Just now</span>
+        `;
+
+        recentMessages.prepend(messageDiv);
+
+        form.reset();
+    });
+
+};
+
+
+
+
+
 document.getElementById("Backtotop").addEventListener("click", function() {
-    this.scrollTo(screenTop)
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 });
